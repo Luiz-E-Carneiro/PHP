f20e50bbff9f1cd05df4aa94ea50d062f1b88c9f
Luiz-E-Carneiro <140028787+Luiz-E-Carneiro@users.noreply.github.com>
2023-12-14 00:01:33 -0300

    Update data_words.php: Added some words

--- 5_LETTERS/data_words.php
+++ 5_LETTERS/data_words.php
@@ -1,9 +1,22 @@
 <?php
-
+//I added some words made of 5 letters I'll use for the game, but i'm still wondering if I should use an API word
 $data = [
-    2 => ['oi'],
-    3 => ['ola'],
-    4 => ['voce'],
+    "apple", "beach", "chair", "dance", "earth",
+    "faith", "glass", "house", "image", "jolly",
+    "knife", "laugh", "music", "never", "olive",
+    "peace", "quick", "rainy", "shiny", "table",
+    "under", "visit", "water", "xerox", "yacht",
+    "zebra", "alarm", "brave", "cloud", "dance",
+    "eagle", "flirt", "grace", "happy", "ideal",
+    "joker", "karma", "lemon", "movie", "nerve",
+    "optic", "quiet", "radio", "sleep","unity", 
+    "vision", "watch", "youth","abide", "blend", 
+    "candy", "decal", "elite", "ivory", "joint",
+    "flame", "globe", "haste", "novel", "oasis",
+    "knack", "lunar", "mirth", "scent", "tramp",
+    "plumb", "quilt", "rider", "zesty", "ultra",
+    "vivid", "wager", 
 ];
 
+// In order to use with JS
 echo json_encode($data);
